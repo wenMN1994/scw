07cbd5356850b7d940b13b428d46b83a83f214ae
温文星 <18475536452@163.com>
2019-12-21 09:55:01 +0800

    搭建webui项目

---
+++ scw-webui/src/main/java/com/dragon/scw/ScwWebuiApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.scw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScwWebuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScwWebuiApplication.class, args);
+	}
+
+}
