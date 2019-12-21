9d7a91020a5b7396ac717d6d782a817a9240e65e
温文星 <18475536452@163.com>
2019-12-21 16:31:33 +0800

    抽取公共代码

--- scw-webui/src/main/java/com/dragon/scw/controller/DispatcherController.java
+++ scw-webui/src/main/java/com/dragon/scw/controller/DispatcherController.java
@@ -1,7 +1,6 @@
 package com.dragon.scw.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
