79656af9dbd1927556b63304677e7be232738814
温文星 <18475536452@163.com>
2019-12-21 16:39:57 +0800

    重名名包名

--- scw-webui/src/main/java/com/dragon/scw/webui/controller/DispatcherController.java
+++ scw-webui/src/main/java/com/dragon/scw/webui/controller/DispatcherController.java
@@ -1,4 +1,4 @@
-package com.dragon.scw.controller;
+package com.dragon.scw.webui.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
